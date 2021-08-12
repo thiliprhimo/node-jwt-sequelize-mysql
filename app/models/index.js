a19c73f48de75ba23285e23b945c94b813e54b9b
Thilip Rhimo <88575534+thiliprhimo@users.noreply.github.com>
2021-08-12 16:23:15 +0530

    Add files via upload

--- app/models/index.js
+++ app/models/index.js
@@ -20,18 +20,15 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.user = require("../models/user.model")(sequelize, Sequelize);
-db.role = require("../models/role.model")(sequelize, Sequelize);
+db.user = require("../models/user.model.js")(sequelize, Sequelize);
+db.role = require("../models/role.model.js")(sequelize, Sequelize);
 
-db.role.belongsToMany(db.role, {
-  as: "access",
+db.role.belongsToMany(db.user, {
   through: "user_roles",
   foreignKey: "roleId",
   otherKey: "userId",
 });
-
-db.user.belongsToMany(db.user, {
-  as: "access",
+db.user.belongsToMany(db.role, {
   through: "user_roles",
   foreignKey: "userId",
   otherKey: "roleId",
