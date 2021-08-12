a549b6dfeee93a21789465def2edc13f2f4fe743
Thilip Rhimo <88575534+thiliprhimo@users.noreply.github.com>
2021-08-12 16:20:49 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -17,6 +17,29 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const db = require("./app/models");
 db.sequelize.sync();
 
+// db.sequelize.sync({ force: true }).then(() => {
+//   console.log("Drop and Resync Db");
+//   initial();
+// });
+// const Role = db.role;
+
+// function initial() {
+//   Role.create({
+//     id: 1,
+//     name: "user",
+//   });
+
+//   Role.create({
+//     id: 2,
+//     name: "moderator",
+//   });
+
+//   Role.create({
+//     id: 3,
+//     name: "admin",
+//   });
+// }
+
 app.get("/", (req, res) => {
   res.json("Thanks for visiting Rhimo's (JWT) application");
 });
